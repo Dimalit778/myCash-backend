53cbdb70cddf9e4f6b1f00aa2afc21a161632790
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-02-14 03:42:40 +0200

    a

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,7 +10,7 @@ export const generateToken = (res, id) => {
   console.log(token);
   res.cookie('access_token', token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    secure: process.env.NODE_ENV !== 'production', // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 60000, // 30 days
   });
@@ -22,7 +22,7 @@ export const generateRefreshToken = (res, id) => {
   });
   res.cookie('refresh_token', refresh_token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    secure: process.env.NODE_ENV !== 'production', // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
