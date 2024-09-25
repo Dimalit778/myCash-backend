704f2cdf6b3d46fff0ea85f7b758e96e0189cee0
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-09-25 16:07:56 +0300

    as

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 // !--> error Handler <-- //
-import { errorHandler } from './middleware/errorMiddleware.js';
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 //@  --> routes import <-- //
-import userRoute from './routes/userRoute.js';
-import authRoute from './routes/authRoute.js';
-import transactionsRoute from './routes/transactionsRoute.js';
-import { verifyToken } from './middleware/authMiddleware.js';
+import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
+import transactionsRoute from "./routes/transactionsRoute.js";
+import { verifyToken } from "./middleware/authMiddleware.js";
 
 const app = express();
 app.use(cookieParser());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -27,51 +27,49 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: 'https://mycash-ra2a-yxco.onrender.com',
-    // origin: 'http://localhost:3000',
+    origin: "https://mycash-ra2a-yxco.onrender.com",
+
     credentials: true,
   })
 );
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 // * --> ALL ROUTES <-- //
 
 //@ ---- { User Routes } ---- //
-app.use('/api/users', verifyToken, userRoute);
+app.use("/api/users", verifyToken, userRoute);
 //@ ---- { Auth Routes } ---- //
-app.use('/api/auth', authRoute);
+app.use("/api/auth", authRoute);
 //@ ---- { transactions Expenses & Incomes Routes } ---- //
-app.use('/api/transactions', verifyToken, transactionsRoute);
+app.use("/api/transactions", verifyToken, transactionsRoute);
 // Re render Render Service
-app.use('/api/renderSite', (req, res) => {
-  res.send('renderSite');
+app.use("/api/renderSite", (req, res) => {
+  res.send("renderSite");
 });
 
 /// -- connection to DB
 const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose
   .connect(process.env.DB, dbOptions)
-  .then(() => console.log('DB store connected!'))
+  .then(() => console.log("DB store connected!"))
   .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('running on port ' + port);
-  console.log('Connected to backend');
+  console.log("running on port " + port);
+  console.log("Connected to backend");
 });
 // -----> FOR PRODUCTION
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
-  const static_path = path.resolve(__dirname, 'front', 'build');
+  const static_path = path.resolve(__dirname, "front", "build");
   app.use(express.static(path.join(static_path)));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(static_path, 'index.html'))
-  );
+  app.get("*", (req, res) => res.sendFile(path.resolve(static_path, "index.html")));
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  app.get("/", (req, res) => {
+    res.send("API is running....");
   });
 }
 // app.use(notFound);
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,32 +1,31 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // --- Generate access Token
 export const generateToken = (res, id) => {
   const token = jwt.sign({ id }, process.env.JWT, {
-    expiresIn: '30m',
+    expiresIn: "30m",
   });
 
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     // path: '/',
     expires: new Date(Date.now() + 10000 * 30),
     secure: true, // Use secure cookies in production
     httpOnly: true,
-    sameSite: 'None', // Prevent CSRF attacks
+    sameSite: "None", // Prevent CSRF attacks
   });
 };
 // --- Generate refresh Token
 export const generateRefreshToken = (res, id) => {
   const refToken = jwt.sign({ id }, process.env.REFRESH_JWT, {
-    expiresIn: '30d',
+    expiresIn: "30d",
   });
-  res.cookie('refToken', refToken, {
-    path: '/',
+  res.cookie("refToken", refToken, {
+    path: "/",
     httpOnly: true,
-
-    sameSite: 'None', // Prevent CSRF attacks
+    sameSite: "None", // Prevent CSRF attacks
     secure: true, // Use secure cookies in production
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
